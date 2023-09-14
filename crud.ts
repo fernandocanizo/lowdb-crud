23f3d1bffdae19ab9b8294f4a9399d7f78b4fce9
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-09-14 17:10:56 -0300

    Closes #1: Implement all planned CRUD functions

--- crud.ts
+++ crud.ts
@@ -1,33 +1,68 @@
+import db from './db.js'
 import { Book } from './types/Book.js'
 
-export const create = async (newBook: Book) : boolean => {
-  return false
+const { bookList } = db.data
+
+export const create = async (newBook: Book) : Promise<boolean> => {
+  try {
+    bookList.push(newBook)
+    await db.write()
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
 }
 
-export const readAll = async () : Book[] => {
-  return []
+export const readAll = () : Book[] => {
+  return bookList
 }
 
-export const findByTitle = async (title: string) : Book | undefined => {
-  return undefined
+export const findByTitle = (title: string) : Book | undefined => {
+  return bookList.find((book: Book) => book.title === title)
 }
 
-export const findByAuthor = async (author: string) : Book | undefined => {
-  return undefined
+export const findByAuthor = (author: string) : Book | undefined => {
+  return bookList.find((book: Book) => book.author === author)
 }
 
-export const update = async (modifiedBook: Book) : boolean => {
-  return false
+export const update = async (modifiedBook: Book, index: number) : Promise<boolean> => {
+  // since there's no IDs, there's no possible way to ensure we find the
+  // proper book, so the only way to make an update is to receive the index.
+  // Very ugly solution :/
+  try {
+    bookList.splice(index, 1, modifiedBook)
+    db.write()
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
 }
 
-export const upsert = async (book: Book) : boolean => {
-  return false
+// I had an `upsert` function planned, but there's no point in doing it for
+// the reasons pointed out in the `update` function comments
+
+export const deleteByAuthor = async (author: string) : Promise<boolean> => {
+  try {
+    const newBookList: Book[] = bookList.filter((book: Book) => book.author !== author)
+    db.data.bookList = newBookList
+    await db.write()
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
 }
 
-export const deleteByAuthor = async (author: string) : boolean => {
-  return false
-}
-
-export const deleteByTitle = async (title: string) : boolean => {
-  return false
+export const deleteByTitle = async (title: string) : Promise<boolean> => {
+  try {
+    const newBookList: Book[] = bookList.filter((book: Book) => book.title !== title)
+    db.data.bookList = newBookList
+    await db.write()
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
 }
